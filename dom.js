ab61dbae47c141735d46fc36a6aea6d707d07d85
lausellv <lausellv@yahoo.com>
2020-11-07 14:55:43 +0100

    add CF code

--- dom.js
+++ dom.js
@@ -241,6 +241,31 @@ target.classList.toggle('button--red');
 target.classList.toggle('button--green');
  });
 
+ let extraContent = document.querySelector('.extra-content');
+
+let showMoreButton = document.querySelector('.show-more-button');
+
+let showLessButton = document.querySelector('.show-less-button');
+
+showMoreButton.addEventListener('click', function(event){
+  // Reveal the extra content
+  extraContent.classList.add('visible');
+  // Hide `Show More` button
+  event.target.classList.remove('visible');
+  // Show `Show Less` button
+  showLessButton.classList.add('visible');
+});
+
+showLessButton.addEventListener('click', function(){
+  // Hide the extra content
+  extraContent.classList.remove('visible');
+  // Hide `Show Less` button
+  event.target.classList.remove('visible');
+  // Show `Show More` button
+  showMoreButton.classList.add('visible');
+});
+
+
 // //   // console.log(e.target);
 // //   // console.log(e.target.id);
 // //   // console.log(e.target.className);
